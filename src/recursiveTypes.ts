c3c74450e75c8c4cb5a1b8d1a5ade9c053c3d153
Luca A. Müller <70109317+lucaired@users.noreply.github.com>
2024-03-11 07:25:34 +0000

    next iteration of flattenList

--- src/recursiveTypes.ts
+++ src/recursiveTypes.ts
@@ -1,10 +1,13 @@
 import assert from "assert";
 
-type LinkedList<T> = { value: T | undefined } & { next: LinkedList<T> | null };
+type LinkedList<T> = { value: T | T[] | undefined } & { next: LinkedList<T> | null };
 
 const list = { value: 1, next: { value: 2, next: { value: 3, next: null } } };
 
-function printList<T>(node: LinkedList<T>) {
+function printList<T>(node: LinkedList<T> | null) {
+  if (node === null) {
+    return;
+  }
   console.log(node.value);
   if (node.next !== null) {
     printList(node.next);
@@ -33,6 +36,7 @@ printList(list);
  * @returns the flattened list
  */
 function flattenList<T>(node: LinkedList<T>): LinkedList<T> | null {
+
   function flattenNode<T>(node: LinkedList<T>): LinkedList<T> | null {
     if (node.value instanceof Array) {
       // {value: undefined, next: { value: <value>, next: {} }}
@@ -52,8 +56,29 @@ function flattenList<T>(node: LinkedList<T>): LinkedList<T> | null {
       return node;
     }
   }
-  return flattenNode(node);
+
+  /**
+   * We traverse the list and flatten each node recursively:
+   * - if the next element is null, we return the node and flatten its value
+   * - if the next element is not null, we flatten the current value and append the next element
+   * 
+   * { value: 1, next: { value: [2,3,4], next: { value: 5, next: null } }
+   * 
+   * @param node 
+   * @returns flattened list
+   */
+  function traverseAndFlatten(node: LinkedList<T>): LinkedList<T> | null {
+    if (node.next === null) {
+      return flattenNode(node);
+    } else {
+      const next = traverseAndFlatten(node.next);
+      node.next = next;
+      return flattenNode(node);
+    }
+  }
+  return traverseAndFlatten(node);
 }
+
 const list3 = {
   value: [1, 2, 3],
   next: null,
@@ -72,5 +97,13 @@ assert.deepStrictEqual(flattenList(emptyList), {
   value: undefined,
   next: null,
 });
+const nestedList = {
+  value: 1, next: {
+    value: [2, 3, 4], next: { value: 5, next: null }
+  }
+};
+printList(flattenList(nestedList));
+assert.strictEqual(flattenList(nestedList), { value: 1, next: { value: 2, next: { value: 3, next: { value: 4, next: { value: 5, next: null } } } } });
+console.log("Tested %j ➡️ 👍", nestedList);
 
 console.log("All assertions passed");
